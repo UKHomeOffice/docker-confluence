85f7f97ce1c042377206c588a972bc225696929b
Allan Degnan <allan@adegnan.net>
2015-09-03 13:42:27 +0100

    removed startup error, not sure why it wants hat folder

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@
 # Add our cron
 echo "${CRON_TIME} ${CRON_USER} ${CRON_CMD} > /dev/stdout 2>&1" >> /etc/crontab
 
-# Fix Confluence config.
+# Startup whines that this folder hasn't been created
+mkdir -p /usr/local/Confluence/conf/Standalone/localhost
 
+# Fix Confluence DB conf
+
+# Fix Confluence HTTP conf
 
 # Run Supervisor as pid 1.  
 # Typically you want to just run the app directly, but we're also running cron for backups.

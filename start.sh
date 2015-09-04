264d56562ef48adf9a448ff6c4fac2c74566324f
Allan Degnan <allan@adegnan.net>
2015-09-04 12:16:24 +0100

    fixed permissions error when mounting ceph store

--- start.sh
+++ start.sh
@@ -6,8 +6,11 @@ echo "${CRON_TIME} ${CRON_USER} ${CRON_CMD} > /dev/stdout 2>&1" >> /etc/crontab
 # Startup whines that this folder hasn't been created
 mkdir -p /usr/local/Confluence/conf/Standalone/localhost
 
-# Fix Confluence Tomcat conf
+# Make sure confluence owns the coluence directory
+mkdir -p ${APP_DATA}
+chown -R ${APP_USER}:${APP_USER} ${APP_DATA} 
 
+# Fix Confluence Tomcat conf; Don't think we need this
 
 # Run Supervisor as pid 1.  
 # Typically you want to just run the app directly, but we're also running cron for backups.

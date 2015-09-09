7f2018b03ac39b506a882f11c2f1abeb31f1a07e
Allan Degnan <allan@adegnan.net>
2015-09-09 14:51:37 +0100

    fixed ssl redir

--- start.sh
+++ start.sh
@@ -9,10 +9,7 @@ chown -R ${APP_USER}:${APP_USER} ${APP_DATA}
 
 # Fix Confluence Tomcat conf
 if [ -n "$SSL_REVERSE_PROXY_NAME" ]; then
-    sed 's/disableUploadTimeout="true"\/>/disableUploadTimeout="true"\n                secure="true"\/>/' -i ${APP_PATH}/conf/server.xml
-    sed 's/disableUploadTimeout="true"/disableUploadTimeout="true"\n                   proxyPort="'"${SSL_REVERSE_PROXY_PORT}"'"/' -i ${APP_PATH}/conf/server.xml
-    sed 's/disableUploadTimeout="true"/disableUploadTimeout="true"\n                   proxyName="'"$(echo ${SSL_REVERSE_PROXY_NAME} | sed -e 's/[\/&]/\\&/g')"'"/' -i ${APP_PATH}/conf/server.xml
-    sed 's/disableUploadTimeout="true"/disableUploadTimeout="true"\n                   scheme="https"/' -i ${APP_PATH}/conf/server.xml
+    sed 's/redirectPort="8443"/scheme="https"\n\t secure="true"\n\t proxyName="'"${SSL_REVERSE_PROXY_NAME}"'"\n\t proxyPort="'"${SSL_REVERSE_PROXY_PORT}"'"/' -i ${APP_PATH}/conf/server.xml
 fi
 
 
